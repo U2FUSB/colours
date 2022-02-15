acca948ef721c12b008455bf5a792d437e3637d0
niklas7U <20099542@mail.wit.ie>
2022-02-15 22:36:20 +0000

    FIN Project is finished
    removed unnecessary comments

--- src/main/kotlin/Colours.kt
+++ src/main/kotlin/Colours.kt
@@ -3,57 +3,43 @@ class Colours {
     private val colours:List<String> = listOf("Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet", "Black", "Silver")
 
     fun getAll (): List<String>{
-        //return the colours list
         return colours
     }
 
     fun numberOfColours (): Int{
-        //return the size of the colours list
         return colours.size
     }
 
     fun firstColour() : String{
-        //return the first colour in the list
         return colours.first()
     }
 
     fun lastColour() : String{
-        //return the last colour list
         return colours.last()
     }
 
     fun coloursInAplhabeticOrder() : List<String>{
-        //return the colours in alphabetical order
         //Variation in lambda exp: colours.sortedBy { it }
         return colours.sorted()
     }
 
     fun coloursInAllCapitals(): List<String> {
-        //return the colours in ALL CAPS
-        //Variation in lambda exp foreach: does not work with String Templates --> .forEach seems only to work when using it with prints or when using returns --> would only give back first element
-        //"lowCase: ${colours.forEach {print("${it.lowercase()}, ")} }" +
-        //        "\nupCase: ${colours.forEach{print("${it.uppercase()}, ")} }"
         return colours.map { "${it.uppercase()}, ${it.lowercase()}" }
     }
 
     fun coloursStartingWithLetter(letter: Char) : List<String>{
-        //return the colours starting with the letter passed as a parameter
         return colours.filter { it.first() == letter }
     }
 
     fun coloursWithSpecificNumberOfChars(size: Int) : List<String>{
-        //return the colours that have exactly the same number of chars as the number passed as a parameter
         return colours.filter { it.count() == size }
     }
 
     fun coloursWithLessCharsThan(size: Int) : List<String>{
-        //return the colours that have less chars than the number passed as s parameter
         return colours.filter { it.count() < size }
     }
 
     fun isColourInTheList(colour : String) : Boolean{
-        //return true if the colour passed as a parameter is in the list and false otherwise
-        // Note: the search should be case insensitive - if blue is passed as a parameter it should find Blue, BLUE etc.
         return colours  .map { it.uppercase() }
                         .contains(colour.uppercase())
     }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,6 @@
-//INSTRUCTIONS:
-//  1. Now that you have the code in IntelliJ on your computer, use VCS to "Share this project to your GitHub repo
-//     that is attached to your student email.  MAKE SURE YOU HAVE THIS REPO SET TO PRIVATE.
-//  2. Run the app and familiarise yourself with the output and where each part of the output came from in the code.
-//  3. There are multiple TO DO comments in the two classes.  Follow this workflow when addressing EACH one:
-//       a. Locate a single TO DO item that you want to work on
-//       b. Add the required code to address it.
-//       c. Commit the change to your local repository, making sure your commit message explains (briefly) what you did.
-//       d. Push the change to your remote repository
-//  4. REMEMBER TO USE THIS GITHUB WORKFLOW FOR EACH CHANGE - failure to use GitHub in this way will result in
-//     a lower grade for the assignment as it is a software development tooling module.
-//  5. NOTE that you should not change the function names, parameters or return types when working on the fixes - to
-//     do so will also result in a reduced mark.
-
 private val colours = Colours()
 
 fun main(){
-
-    //Use String templates i.e. ${} to insert the method calls into the actual println string.
-    // tidy up the printing layout so that it is neat and has a nicer UI - you can choose what you would like to do
-    //     here, maybe add breaks, lines, tabs, dividers, etc, whatever you think for making it nicer / easier to read.
     println("""
         |The list is: ${colours.getAll()}
         |--------------------------------------------------------------
@@ -45,7 +27,7 @@ fun main(){
 
     colours.getAll().forEach{ println("Colour: $it")}
 
-    //for the three additional functions you wrote in Colours.kt, print the returned value here.
+    // The three additional Print-Statements
     println("\nThe Colours: ${colours.colourContainsLetter('l')} all contain the letter 'l' ")
     println("The First half of the colours are: ${colours.firstHalf()}")
     println("The Second half of the colours are: ${colours.secondHalf()}")
